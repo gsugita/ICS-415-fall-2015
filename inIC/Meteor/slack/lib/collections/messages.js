a2e1e46407c4938cf0dcfd647c66781ff7138ebe
gsugita <gsugita6@hawaii.edu>
2015-10-07 13:05:29 -0700

    inClass 7

---
+++ inIC/Meteor/slack/lib/collections/messages.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Falcon on 10/7/2015.
+ */
+Messages = new Mongo.Collection("messages");
+Messages.insert ({greeting: "hello"},function(){});
+
+Meteor.startup(function () {
+    if (Messages.find().count() === 0) {
+        for( var i = 0; i < 10; i++ ) {
+            Messages.insert({text: "A dummy message"});
+        }
+    }
+});
---
+++ inIC/Meteor/slack/server/seeder.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Falcon on 10/7/2015.
+ */
+Meteor.startup(function() {
+    Factory.define('message', Messages, {
+        text: function() {
+            return Fake.sentence();
+        }
+    });
+
+    // Add this if you want to remove all messages before seeding
+    Messages.remove({});
+
+    if (Messages.find({}).count() === 0) {
+        _(10).times(function(n) {
+            Factory.create('message');
+        });
+    }
+});
